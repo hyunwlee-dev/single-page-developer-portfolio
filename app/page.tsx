9ce6d51bc76cb10276738523f96443f3525478f8
hyunwlee-dev <hwlee3928@gmail.com>
2024-02-15 20:13:55 +0900

    add: theme.css

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main>
       <h1 className={styles["font-medium"]}>SpaceGrotesk-Bold</h1>
-      <p>SpaceGrotesk-Medium</p>
-      <p>SpaceGrotesk-VariableFont_wght</p>
+      <h2>SpaceGrotesk-Medium</h2>
+      <h3>SpaceGrotesk-VariableFont_wght</h3>
+      hi there! from hyunwlee
     </main >
   );
 }
